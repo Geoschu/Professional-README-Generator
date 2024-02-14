cac8cfa56d686f0d9b55b319482da876c3f0bbef
George Schultz <georgeschultz@outlook.com>
2024-02-13 22:17:20 -0800

    added files

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
+import generateMarkdown from "./utils/generateMarkdown";
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you located?'
+    },
+    {
+      type: 'input',
+      name: 'bio',
+      message: 'Tell us a bit about yourself.'
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub URL.'
+    }
+  ];
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
